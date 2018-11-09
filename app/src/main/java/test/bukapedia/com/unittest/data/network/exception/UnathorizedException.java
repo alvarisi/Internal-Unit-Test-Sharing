49282896e747bedb8c959976c7f36c68b1be03cc
alvarisi <alvarisi01@gmail.com>
2018-11-09 08:52:10 +0700

    add exception

---
+++ app/src/main/java/test/bukapedia/com/unittest/data/network/exception/UnathorizedException.java
@@ -0,0 +1,8 @@
+package test.bukapedia.com.unittest.data.network.exception;
+
+import java.io.IOException;
+
+public class UnathorizedException extends IOException {
+    public UnathorizedException() {
+    }
+}
