2d5d5eb3bb0a8d931f66e07036858cea4b92d20f
alvarisi <alvarisi01@gmail.com>
2018-11-09 08:52:54 +0700

    source attr

--- app/src/main/java/test/bukapedia/com/unittest/data/network/entity/ArticleResponse.java
+++ app/src/main/java/test/bukapedia/com/unittest/data/network/entity/ArticleResponse.java
@@ -1,14 +1,22 @@
 package test.bukapedia.com.unittest.data.network.entity;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 public class ArticleResponse {
+    @SerializedName("source")
+    @Expose
+    private String source;
     @SerializedName("articles")
     List<ArticleEntity> articles;
 
     public List<ArticleEntity> getArticles() {
         return articles;
+    }
+
+    public String getSource() {
+        return source;
     }
 }
